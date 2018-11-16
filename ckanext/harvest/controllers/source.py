8739796f3cfc58cd763f2f760263f00e3edbc792
DaleDMT <daledmt@gmail.com>
2018-11-16 14:54:23 +0300

    Access to harvesting source for sysadmin only

---
+++ ckanext/harvest/controllers/source.py
@@ -0,0 +1,10 @@
+from ckan.controllers.package import PackageController
+from ckan.common import c
+from ckan.lib.base import abort
+
+
+class SourceController(PackageController):
+    def new(self, data=None, errors=None, error_summary=None):
+        if not c.userobj or not c.userobj.sysadmin:
+            abort(404)
+        return super(SourceController, self).new(data, errors, error_summary)
