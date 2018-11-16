82aad7a40e36ffcccda9f4515f8d6d9b08f91d57
DaleDMT <daledmt@gmail.com>
2018-11-16 19:31:41 +0300

    deleted sources access

--- ckanext/harvest/controllers/source.py
+++ ckanext/harvest/controllers/source.py
@@ -14,9 +14,9 @@ class SourceController(PackageController):
         return super(SourceController, self).new(data, errors, error_summary)
 
     def show_deleted(self):
-        # TODO: Access
+        if not c.userobj or not c.userobj.sysadmin:
+            abort(404)
 
-        # sources = tk.get_action('harvest_source_list')({}, {})
         sources = [a.__dict__ for a in DGUAHarvesterSource.get_deleted_sources().all()]
         context = {
             'sources': sources
